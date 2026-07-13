aaacbfadf8fa8bfbec2a79eebe577b94847ca2a7
Julian Andres Tovar Betancourt <jatovarbe@unadvirtual.edu.co>
2026-07-13 17:14:12 -0500

    main

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Archivo principal del proyecto
+Juego: Cazador de Contraseñas
+"""
+
+from juego_cazador import JuegoCazador
+
+
+def main():
+    juego = JuegoCazador()
+    juego.jugar()
+
+
+if __name__ == "__main__":
+    main()
